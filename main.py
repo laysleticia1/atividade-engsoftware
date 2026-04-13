8849e236932b70b5151d1d43eaa7bf84e8091f65
laysleticia1 <laysleticia200@gmail.com>
2026-04-13 11:24:38 -0300

    Quarto commit: implementando menu interativo

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-tarefas = []
+def menu():
+    while True:
+        print("\n1 - Adicionar tarefa")
+        print("2 - Listar tarefas")
+        print("3 - Sair")
 
-def adicionar_tarefa():
-    tarefa = input("Digite uma tarefa: ")
-    tarefas.append(tarefa)
-    print("Tarefa adicionada!")
+        opcao = input("Escolha uma opção: ")
 
-def listar_tarefas():
-    print("\nLista de tarefas:")
-    for i, tarefa in enumerate(tarefas, start=1):
-        print(f"{i}. {tarefa}")
+        if opcao == "1":
+            adicionar_tarefa()
+        elif opcao == "2":
+            listar_tarefas()
+        elif opcao == "3":
+            break
+        else:
+            print("Opção inválida!")
+
+menu()
