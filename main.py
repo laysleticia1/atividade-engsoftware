6e5f691dbbefe6e71612ecb94c81dcdd32df841b
laysleticia1 <laysleticia200@gmail.com>
2026-04-13 11:23:57 -0300

    Terceiro commit: adicionando listagem de tarefas

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def adicionar_tarefa():
     tarefa = input("Digite uma tarefa: ")
     tarefas.append(tarefa)
     print("Tarefa adicionada!")
+
+def listar_tarefas():
+    print("\nLista de tarefas:")
+    for i, tarefa in enumerate(tarefas, start=1):
+        print(f"{i}. {tarefa}")
